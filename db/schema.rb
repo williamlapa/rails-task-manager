2ef1cc613f9e4e63abdfb78d69246c5ca8fd9b69
william lapa <williamlapa@hotmail.com>
2020-11-26 14:03:44 -0300

    tasks ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_26_152530) do
+ActiveRecord::Schema.define(version: 2020_11_26_165114) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "details"
-    t.boolean "completed"
+    t.boolean "completed", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end

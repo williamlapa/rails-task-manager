cb3d06d26e870158c6406f23e4f257fd43ed72ef
william lapa <williamlapa@hotmail.com>
2020-11-26 14:58:33 -0300

    tasks ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'controller#task'
+  get '/tasks', to: 'tasks#index'
 end

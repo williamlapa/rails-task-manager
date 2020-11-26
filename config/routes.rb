a318cab6fd8601ccdc5d2681043b4b50f374ed5a
william lapa <williamlapa@hotmail.com>
2020-11-26 17:50:11 -0300

    FRONT END PENDENTE

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/tasks', to: 'tasks#index'
+
+  # # As a user, I can create a restaurant
+  get '/tasks/new', to: 'tasks#new', as: :new_task # render the form
+  post '/tasks', to: 'tasks#create'
+
+  # # As a user, I can edit a restaurant
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task # mostrar o form pro usario
+  patch '/tasks/:id', to: 'tasks#update'
+
+  # As a user, I can delete a restaurant
+  delete 'tasks/:id', to: 'tasks#destroy', as: :apagar
+
   get '/tasks/:id', to: 'tasks#show', as: :task
 end
